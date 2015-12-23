5b3602b460a6d9e0dfd1b8f831c5844a6e89e4ad
tomcat1022jp <tomcat1022jp@gmail.com>
2015-12-23 13:39:21 +0000

    fixed horizontal form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
     @messages = Message.all
   end
 
   ## ここから追記
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save 
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else#メッセージが保存出来なかったとき
+      @message = Message.all
+      flash.now[:aleart] = "メッセージの保存に失敗しました"
+      render 'index'
+    end
   end
 
   private
   def message_params
+    #binding.pry
     params.require(:message).permit(:name, :body)
   end
   ## ここまで
