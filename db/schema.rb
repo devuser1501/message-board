e37e0ac28b594c17037400f3f6731b4ed7910bca
tomcat1022jp <tomcat1022jp@gmail.com>
2016-01-04 04:06:14 +0000

    challenge2_age

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203053614) do
+ActiveRecord::Schema.define(version: 20151228191633) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
